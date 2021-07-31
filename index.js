57f84272f9c5a044ff65d3f685811912c21264f4
rana <ashikex49@gmail.com>
2021-07-31 20:20:06 +0600

    light

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-for (let i = 0; i < 10; i++) {
-  console.log(i);
+function updateLight(current) {
+  // 8kyu
+  const lights = ['green', 'yellow', 'red'];
+  let index = lights.indexOf(current);
+
+  let getInd = index === lights.length - 1 ? 0 : index + 1;
+  return lights[getInd];
 }
+
+console.log(updateLight('red'));
