1df0a59623fd918f2cd43b8d6e57858be3b69f54
rana <ashikex49@gmail.com>
2021-08-01 21:16:03 +0600

    bol

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
-function multiTable(num) {
-  return [...Array(10)]
-    .map((_, i) => `${i + 1} * ${num} = ${num * (i + 1)}`)
-    .join('/n');
-}
+// remove falsy value from array (Task)
+// six falsy values in JS,they are
+// "" (empty string)
+// null
+// NaN
+// false
+// undefined
+// 0 (zero)
 
-console.log(multiTable(7));
+const values = [
+  'akib',
+  null,
+  'zara',
+  0,
+  NaN,
+  '',
+  10,
+  undefined,
+  'ashik',
+  true,
+  false,
+];
+
+// way one
+
+const newArr = [];
+
+values.forEach((dt) => {
+  if (dt) {
+    newArr.push(dt);
+  }
+});
+
+console.log(newArr);
+
+// way two
+
+const result = values.filter((dt) => Boolean(dt));
+// you can also do it  another way
+
+const another = values.filter(Boolean);
+console.log(another);
+
+console.log(result);
