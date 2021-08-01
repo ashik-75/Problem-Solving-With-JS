e5685c1028385825a31bb4e66266da18f094e4ee
rana <ashikex49@gmail.com>
2021-08-01 21:39:44 +0600

    f

--- index.js
+++ index.js
@@ -1,44 +1,7 @@
-// remove falsy value from array (Task)
-// six falsy values in JS,they are
-// "" (empty string)
-// null
-// NaN
-// false
-// undefined
-// 0 (zero)
+// convert anyting to boolean
 
-const values = [
-  'akib',
-  null,
-  'zara',
-  0,
-  NaN,
-  '',
-  10,
-  undefined,
-  'ashik',
-  true,
-  false,
-];
+function print(dt) {
+  console.log(Boolean(dt));
+}
 
-// way one
-
-const newArr = [];
-
-values.forEach((dt) => {
-  if (dt) {
-    newArr.push(dt);
-  }
-});
-
-console.log(newArr);
-
-// way two
-
-const result = values.filter((dt) => Boolean(dt));
-// you can also do it  another way
-
-const another = values.filter(Boolean);
-console.log(another);
-
-console.log(result);
+print('');
