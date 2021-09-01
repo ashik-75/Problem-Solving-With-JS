98edf028ba6b7bb073f2d54369a9260a5274cae1
rana <ashikex49@gmail.com>
2021-09-01 20:45:59 +0600

    new alternate case

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-const length = 100;
+String.prototype.toAlternate = function () {
+  return this.split('')
+    .map((dt) =>
+      dt.toLowerCase() === dt ? dt.toUpperCase() : dt.toLowerCase()
+    )
+    .join('');
+};
 
-const array = [...Array(length).keys()];
+console.log('testFued2343 suEd54'.toAlternate());
 
-const { performance } = require('perf_hooks');
-
-const start = performance.now();
-
-for (let i = 0; i < array.length; i++) {
-  console.log(i);
-}
-
-const end = performance.now();
-
-console.log('first ', end - start);
-
-// another
-
-const anStart = performance.now();
-
-for (let i = 0; i < length; i++) {
-  console.log(i);
-}
-const anEnd = performance.now();
-
-console.log('second ', anEnd - anStart);
+console.log('1'.toUpperCase());
