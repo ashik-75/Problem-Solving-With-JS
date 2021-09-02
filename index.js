f200150170e4773d16ae3c2d0ed1bc8f5f3e9031
rana <ashikex49@gmail.com>
2021-09-02 23:36:05 +0600

    total amount of point

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-function divisibleBy(numbers, divisor) {
-  return numbers.filter((num) => num % divisor === 0);
+function points(games) {
+  let totalPoint = 0;
+  let data = games.map((dt) => dt.split(':'));
+
+  data.map((dt) => {
+    if (dt[0] > dt[1]) {
+      totalPoint += 3;
+    } else if (dt[0] === dt[1]) {
+      totalPoint += 1;
+    }
+  });
+
+  return totalPoint;
+  // your code here
 }
 
-console.log(divisibleBy([0, 1, 2, 3, 4, 5, 6], 4));
+console.log(
+  points(['1:0', '2:0', '3:0', '4:0', '2:1', '1:3', '1:4', '2:3', '2:4', '3:4'])
+);
