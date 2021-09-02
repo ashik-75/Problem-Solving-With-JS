4e5c901a28f12d4785bc2809436021c4a3633b1f
rana <ashikex49@gmail.com>
2021-09-02 23:26:30 +0600

    divisible by given number

--- index.js
+++ index.js
@@ -1,33 +1,5 @@
-function validate(number) {
-  let data = String(number).split('').map(Number);
-
-  if (data.length % 2 === 0) {
-    data = data.map((dt, index) => {
-      if (index % 2 === 0) {
-        return dt * 2;
-      } else {
-        return dt;
-      }
-    });
-  } else {
-    data = data.map((dt, index) => {
-      if (index % 2 === 1) {
-        return dt * 2;
-      } else {
-        return dt;
-      }
-    });
-  }
-
-  data = data.reduce((acc, curr) => {
-    if (curr > 9) {
-      return acc + (curr - 9);
-    } else {
-      return acc + curr;
-    }
-  }, 0);
-
-  return data % 10 === 0;
+function divisibleBy(numbers, divisor) {
+  return numbers.filter((num) => num % divisor === 0);
 }
 
-console.log(validate(1034));
+console.log(divisibleBy([0, 1, 2, 3, 4, 5, 6], 4));
