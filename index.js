435aa6e2f8b580317f72870d7843eff1cde121c7
rana <ashikex49@gmail.com>
2021-09-02 23:21:07 +0600

    credit card check

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-function sortByLength(array) {
-  return array.sort((a, b) => a.length - b.length);
+function validate(number) {
+  let data = String(number).split('').map(Number);
+
+  if (data.length % 2 === 0) {
+    data = data.map((dt, index) => {
+      if (index % 2 === 0) {
+        return dt * 2;
+      } else {
+        return dt;
+      }
+    });
+  } else {
+    data = data.map((dt, index) => {
+      if (index % 2 === 1) {
+        return dt * 2;
+      } else {
+        return dt;
+      }
+    });
+  }
+
+  data = data.reduce((acc, curr) => {
+    if (curr > 9) {
+      return acc + (curr - 9);
+    } else {
+      return acc + curr;
+    }
+  }, 0);
+
+  return data % 10 === 0;
 }
 
-console.log(sortByLength(['Telescopes', 'Glasses', 'Eyes', 'Monocles']));
+console.log(validate(1034));
